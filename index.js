8cfac9ee24ed984116db76c5262dfb918628c85c
Rizky Faturriza <rfaturriza@gmail.com>
2025-05-24 17:53:35 +0700

    refactor: Remove static file serving middleware for React app

--- index.js
+++ index.js
@@ -28,8 +28,6 @@ connectMongo()
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")));
 // API routes
 app.get("/api", (_, res) => {
   res.json({ message: "I am a live" });
